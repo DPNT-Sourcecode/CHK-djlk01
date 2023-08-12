10fe8dfcc90805766d6688d29ea26c24de6ba076
sbx_user1051 <sbx_user1051@169.254.102.77>
2023-08-12 17:03:17 +0000

    Sat Aug 12 17:03:17 UTC 2023

---
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -0,0 +1,13 @@
+package befaster.solutions.CHK;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class CheckoutSolutionTest {
+
+    private CheckoutSolution checkoutSolution;
+
+    @BeforeEach
+    public void setUp(){
+        checkoutSolution = new CheckoutSolution();
+    }
+}
