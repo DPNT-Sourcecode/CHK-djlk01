376b7952cfc06a7155e8e02a9a55c894c73e7e33
sbx_user1051 <sbx_user1051@169.254.102.77>
2023-08-12 18:25:03 +0000

    Sat Aug 12 18:25:03 UTC 2023

--- src/main/java/befaster/solutions/CHK/SpecialOffer.java
+++ src/main/java/befaster/solutions/CHK/SpecialOffer.java
@@ -2,5 +2,10 @@ package befaster.solutions.CHK;
 
 import lombok.*;
 
-public record SpecialOffer(int quantity, int price) {
+public record SpecialOffer(int quantity, int price, char freeItem) {
+
+    public SpecialOffer(int quantity, int price) {
+        this(quantity, price,)
+    }
 }
+
