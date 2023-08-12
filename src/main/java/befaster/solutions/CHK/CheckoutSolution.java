b6c2c8e75b8c2c4878ca7501e17064b285359fa8
sbx_user1051 <sbx_user1051@169.254.102.77>
2023-08-12 18:22:03 +0000

    Sat Aug 12 18:22:03 UTC 2023

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -1,5 +1,6 @@
 package befaster.solutions.CHK;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,9 +18,13 @@ public class CheckoutSolution {
                 put('E', 40);
             }};
     private static final Map<Character, List<SpecialOffer>> SPECIAL_OFFERS =
-            Map.of('A', List.of(new SpecialOffer(3, 130),
-                            new SpecialOffer(5, 200)) ,
-                    'B', List.of(new SpecialOffer(2, 45)));
+            Map.of('A', Arrays.asList(
+                            new SpecialOffer(5, 200),
+                            new SpecialOffer(3, 130)
+                    ),
+                    'B', Arrays.asList(
+                            new SpecialOffer(2, 45)
+                    ));
 
     public Integer checkout(String skus) {
         //input validation of skus - correct parsing
@@ -33,36 +38,37 @@ public class CheckoutSolution {
 
         var items = new HashMap<Character, Integer>();
 
-        for(Character product : SKU_PRICES.keySet()){
+        for (Character product : SKU_PRICES.keySet()) {
             items.put(product, (int) skus.chars()
                     .filter(ch -> ch == product)
                     .count());
         }
 
-        for(Character product: SPECIAL_OFFERS.keySet()){
+        for (Character product : SPECIAL_OFFERS.keySet()) {
             var offers = SPECIAL_OFFERS.get(product);
             offers.sort((o1, o2) -> Integer.compare(o2.quantity(), o1.quantity()));
-            for(SpecialOffer offer : offers){
+            for (SpecialOffer offer : offers) {
                 total += applyDirectDiscounts(items, offer, product);
             }
         }
 
-        for(Map.Entry<Character, Integer> item : items.entrySet()){
-            total+= item.getValue() * SKU_PRICES.get(item.getKey());
+        for (Map.Entry<Character, Integer> item : items.entrySet()) {
+            total += item.getValue() * SKU_PRICES.get(item.getKey());
         }
 
         return total;
     }
 
-    private static int applyDirectDiscounts(Map<Character, Integer> items, SpecialOffer offer, char product){
+    private static int applyDirectDiscounts(Map<Character, Integer> items, SpecialOffer offer, char product) {
         int discount = 0;
-        if(offer.quantity() > 0){
+        if (offer.quantity() > 0) {
             var numDiscounts = items.get(product) / offer.quantity();
-            discount+= numDiscounts * offer.price();
+            discount += numDiscounts * offer.price();
             items.put(product, items.get(product) - numDiscounts * offer.quantity());
         }
         return discount;
     }
 }
+
 
 
