b4e208e350a1fafba84b25f4cd245107113f16c1
sbx_user1051 <sbx_user1051@169.254.102.77>
2023-08-12 17:36:17 +0000

    Sat Aug 12 17:36:17 UTC 2023

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -1,8 +1,5 @@
 package befaster.solutions.CHK;
 
-import befaster.runner.SolutionNotImplementedException;
-
-import java.util.HashMap;
 import java.util.Map;
 
 public class CheckoutSolution {
@@ -12,28 +9,37 @@ public class CheckoutSolution {
     private static final Map<Character, Integer> SKU_PRICES =
             Map.of('A', 50, 'B', 30, 'C', 20, 'D', 15);
     private static final Map<Character, SpecialOffer> SPECIAL_OFFERS =
-            Map.of('A', new SpecialOffer(3, 130));
+            Map.of('A', new SpecialOffer(3, 130),
+                    'B', new SpecialOffer(2, 45));
+
     public Integer checkout(String skus) {
         //input validation of skus - correct parsing
         // decomposition - handle pricing of individual items and their respective discounts separately
         // extendable - make it easy to update prices or introduce new special offers in the future
-        if(skus == null || !skus.matches(VALID_SKU_PATTERN)) {
+        if (skus == null || !skus.matches(VALID_SKU_PATTERN)) {
             return -1;
         }
 
         var total = 0;
 
-        for(Character product : SKU_PRICES.keySet()){
-            int count =  (int) skus.chars()
+        for (Character product : SKU_PRICES.keySet()) {
+            int count = (int) skus.chars()
                     .filter(ch -> ch == product)
                     .count();
 
-            total+=count* SKU_PRICES.get(product);
+            if(SPECIAL_OFFERS.containsKey(product)){
+                var offer = SPECIAL_OFFERS.get(product);
+
+                while(count >= offer.quantity())
+            }
+
+            total += count * SKU_PRICES.get(product);
         }
 
         return total;
     }
 }
+
 
 
 
